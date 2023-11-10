6a86486a76a53c1ba4fffef90504b3e5f79f73e3
MichGedd <michaelgeddes555@gmail.com>
2023-11-09 19:49:37 -0500

    Added code for sockets where DE10 is client

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <HAL.h>
 
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <strings.h>
+
+#define PORT 8080
+
 int main() {
 
-	HAL *hal = new HAL();
+	/*HAL *hal = new HAL();
 
 	hal->start_HAL();
 	
@@ -11,7 +20,40 @@ int main() {
 	hal->write_pwm2(0);
 	hal->write_pwm3(0);
 
-	hal->end_HAL();
+	hal->end_HAL();*/
+
+	int status, valread, client_fd;
+	struct sockaddr_in serv_addr;
+
+	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Socket creation error \n");
+		return -1;
+	}
+
+	bzero(&serv_addr, sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr("192.168.1.102");
+	serv_addr.sin_port = htons(PORT);
+
+	if(connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
+		printf("Connection with the server failed");
+		return -1;
+	}
+
+	char buff[80];
+	for(;;) {
+		bzero(buff, sizeof(buff));
+		read(client_fd, buff, sizeof(buff));
+
+		printf("From Server : %s", buff);
+
+		if(strncmp("exit", buff, 4) == 0) {
+			break;
+		}
+	}	
+
+	close(client_fd);
 
 	return 0;
 }
